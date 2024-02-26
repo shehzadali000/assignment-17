8eb5c56948cd158f94fe27846e3cd47da2fc8ab6
shehzad ali <sa8800338@gmail.com>
2024-02-26 01:42:39 -0800

    complete my assignment

---
+++ app.js
@@ -0,0 +1,273 @@
+//    CHAPTER 4
+
+//    LEGAL AND ILLEGAL
+
+
+
+1.
+
+
+var x = 10;
+var y = "hello";
+var z = true;
+
+
+
+2.
+
+
+var fullName = "zaheer";
+var full_name = "zaheer";
+var FullName = "zaheer";
+var $fullname = "zaheer";
+var __fullname = "zaheer";
+
+
+// // var 1st name = "babar";
+// // var first name = "babar";
+// // var function = "baber";
+// // var _ _name  = "baber";
+// // var 12456 =  "baber"
+
+
+
+// 3.
+
+
+document.write(
+
+    "<h1>Rules for naming JS Variables</h1> <br />"
+    +
+
+    ""
+    +
+    "Variable names can only contain numbers, $",
+
+
+    "and _ . For example $my_1stVariable </br>",
+
+
+   " Variables must begin with a letter , $ or _. For example $name, _name or name </br>" ,
+   "Variable names are case sensitive</br>",
+
+
+" Variable names should not be JS keywords"
+)
+
+
+
+
+
+
+
+
+
+
+
+//   chapter 5
+
+
+//    MATH EXPRESSION
+
+ 1.
+    var add1 = +prompt(5)
+    var add2 = +prompt(5)
+    document.write("<br />", "Sum of", add1, " and ", add2, " is ", add1 + add2)
+
+
+
+2.
+    var sub1 = +prompt()
+    var sub2 = +prompt()
+    document.write("<br />", "Sum of", sub1, " and ", sub2, " is ", sub1 - sub2)
+
+    var mul1 = +prompt()
+    var mul2 = +prompt("add second number ")
+    document.write("<br />", "Sum of", mul1, " and ", mul2, " is ", mul1 * mul1)
+
+    var div1 = +prompt()
+    var div2 = +prompt()
+    document.write("<br />", "Sum of", div1, " and ", div2, " is ", div1 / div2)
+
+    var mod1 = +prompt()
+    var mod2 = +prompt()
+    document.write("<br />", "Sum of", mod1, " and ", mod2, " is ", mod1 % mod1)
+
+
+
+3.
+
+var num = 5
+var ini =  ++ num + 7 +  -- num +
+
+
+
+document.write( "Value after variable declaration is: ??" + "</br> initial value :" + ini  +
+"<br/> Value after increment is :"+ ini + "</br> value after addition:" + " </br> value after decrement is :"
++ "</br> the reminder is :0");
+
+var ini = 
+
+console.log( ++ num  + 7 + --num /ini);
+
+
+
+
+
+4.
+
+
+
+
+var ticket = 600    
+
+document.write(
+
+    "total cost to buy 5 tickets to a movie is"
+
+)
+ console.log( ticket * 5     )
+
+
+
+
+
+
+5.
+var table = 5 ;
+
+
+
+
+document.write(  
+     
+              "5 x 1 =" , table * 1 , "</br>"  ,
+              "5 x 1 =" , table * 2, "</br>" ,
+              "5 x 1 =" , table * 3, "</br>" ,
+              "5 x 1 =" , table * 4, "</br>" ,
+              "5 x 1 =" , table * 5, "</br>" ,
+              "5 x 1 =" , table * 6, "</br>" ,
+              "5 x 1 =" , table * 7, "</br>" ,
+              "5 x 1 =" , table * 8, "</br>" ,
+              "5 x 1 =" , table * 9, "</br>" ,
+              "5 x 1 =" , table * 10)
+               
+
+
+
+
+
+//  6.
+       var price = 650;
+       var quan =  3;
+       var price2 = 100;
+       var quan2 = 7;
+       var delivery = 100;
+
+
+       document.write( price * quan + quan2 * price2 + delivery);
+
+
+
+
+  7.
+
+
+
+  var total = 980;
+  var obtain = 804;
+
+
+  document.write( total % obtain
+
+  )
+
+
+
+  9.
+
+
+
+  var dollar = 104.80;
+  var riyal = 28;
+
+  console.log( 10 * dollar + riyal * 25);
+
+
+
+
+
+
+  10. 
+
+
+
+
+  console.log ( 5 * 10 / 2)
+
+
+
+
+
+  11.
+
+
+
+  var current = 2024;
+  var birth  = 2001;
+  
+
+
+
+  console.log (current + birth );
+
+
+ 13.
+
+ var snack = "chocolate chip"
+ var age = "25"
+var maxage = "65"
+var amountofperage = "3"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    CHAPTER 6 
+
+//   MATH EXPRESSION
+
+
+
+var a = 10;
+
+var num = ++a
+
+var num = a++
+
+console.log(num)
+
+
+
+
+
